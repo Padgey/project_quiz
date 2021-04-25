12771cb2650df28a84c19ae0afd31030802a2cf7
Philip <philipadgey@gmail.com>
2021-04-25 10:53:23 +0100

    initial HTML and CSS setup

---
+++ script.js
@@ -0,0 +1,35 @@
+//Database
+const questionsDatabase = {
+    citizenshipQuiz: {
+        question01: {
+            question: "What was the last battle between Great Britain and France?",
+            answers: {
+                1: "The Battle of Trafalgar",
+                2: "The Battle of Waterloo",
+                3: "The Battle of Hastings",
+                4: "The Battle of Agincourt",
+                correctAnswer: 2 
+            }
+        },
+        question02: {
+            question: "Which flower is associated with Wales?",
+            answers: {
+                1: "Daffodil",
+                2: "Rose",
+                3: "Shamrock",
+                4: "Thistle",
+                correctAnswer: 1 
+            }
+        },
+        question03: {
+            question: "How often are general elections in the UK?",
+            answers: {
+                1: "Every 3 years",
+                2: "Every 4 years",
+                3: "Every 5 years",
+                4: "Every 6 years",
+                correctAnswer: 3 
+            }
+        }
+    }
+}
