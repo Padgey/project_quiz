cc82babc38ae94a1ec8f2459b67a9c305634a6ae
Philip <philipadgey@gmail.com>
2021-05-03 10:22:56 +0100

    added percentage correct answers tracker

--- script.js
+++ script.js
@@ -35,62 +35,115 @@ const questionsDatabase = {
 };
 
 //Global variables
-currentQuestionNumber = 1;
-numberOfQuestions = 3;
+let currentQuestionNumber = 1;
+let numberOfQuestions = 3;
+let currentQuestionIsUnanswered = true;
+let numAnsweredQuestions = 0;
+let numCorrectAnswers = 0;
 
 //DOM structure
 const question = document.getElementById("question");
 const questionContainer = document.getElementById("questionContainer");
-const answerA = document.getElementById("answerA");
-const answerB = document.getElementById("answerB");
-const answerC = document.getElementById("answerC");
-const answerD = document.getElementById("answerD");
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const four = document.getElementById("four");
 const nextButton = document.getElementById("nextButton");
 
 //Functions
+const resetQuestionStyling = function() {
+    one.style.border = "medium none";
+    one.style.backgroundColor = "rgb(75, 75, 75)";
+    two.style.border = "medium none";
+    two.style.backgroundColor = "rgb(75, 75, 75)";
+    three.style.border = "medium none";
+    three.style.backgroundColor = "rgb(75, 75, 75)";
+    four.style.border = "medium none";
+    four.style.backgroundColor = "rgb(75, 75, 75)";
+};
 const displayNextQuestion = function() {
     if (currentQuestionNumber === numberOfQuestions) {
-        console.log("Reached the end of the quiz")
+        console.log("Reached the end of the quiz");
         return
     } else {
+        console.log("Changing currentQuestionIsUnanswered to true...");
         currentQuestionNumber++;
-        let nextQuestion = generateQuestionHTML(currentQuestionNumber);
-        questionContainer.innerHTML = nextQuestion;
-    }   
+        generateQuestionHTML(currentQuestionNumber);
+        resetQuestionStyling();
+        currentQuestionIsUnanswered = true;
+        return
+    };   
 };
-
 const generateQuestionHTML = function(number) {
-    let question = questionsDatabase.citizenshipQuiz[number].question;
+    let questionText = questionsDatabase.citizenshipQuiz[number].question;
     let answerOne = questionsDatabase.citizenshipQuiz[number].answers.one;
     let answerTwo = questionsDatabase.citizenshipQuiz[number].answers.two;
     let answerThree = questionsDatabase.citizenshipQuiz[number].answers.three;
     let answerFour = questionsDatabase.citizenshipQuiz[number].answers.four;
 
-    const questionHTML = `<h2 id="question">${number}. ${question}</h2>
-    <div id="answersContainer">
-        <div class="possibleAnswer" id="answerA">${answerOne}</div>
-        <div class="possibleAnswer" id="answerB">${answerTwo}</div>
-        <div class="possibleAnswer" id="answerC">${answerThree}</div>
-        <div class="possibleAnswer" id="answerD">${answerFour}</div>
-    </div>`;
+    question.innerHTML = questionText;
+    one.innerHTML = answerOne;
+    two.innerHTML = answerTwo;
+    three.innerHTML = answerThree;
+    four.innerHTML = answerFour;
 
+    let questionHTML = document.getElementById("questionContainer");
     console.log(questionHTML);
-
-    return questionHTML
+    return
 };
-
+const updateProgress = function() {
+    //Progress Bar
+    //Score 
+    let percentageCorrect = Math.round(numCorrectAnswers / numAnsweredQuestions * 100);
+    let progressString = 
+        `Question ${currentQuestionNumber} of ${numberOfQuestions} | Score - ${numCorrectAnswers} out of ${numAnsweredQuestions} - ${percentageCorrect}%`;
+    progressPercentage.innerHTML = progressString;
+};
 const checkAnswer = function(selectedAnswer) {
-    let correctAnswer = questionsDatabase.citizenshipQuiz[currentQuestionNumber].answers.correctAnswer
-    console.log(`selectedAnswer: ${selectedAnswer}`);
-    console.log(`correctAnswer: ${correctAnswer}`);
+    let correctAnswer = questionsDatabase.citizenshipQuiz[currentQuestionNumber].answers.correctAnswer;
+    if (selectedAnswer !== correctAnswer) {
+        console.log("Incorrect answer!");
+        numAnsweredQuestions++;
+        return false
+    } else {
+        console.log("Correct answer!");
+        numAnsweredQuestions++;
+        numCorrectAnswers++;
+        return true
+    };
 };
+const executeAnswer = function(selectedAnswer) {
+    if (currentQuestionIsUnanswered) {
+        let answerIsCorrect = checkAnswer(selectedAnswer);
+        let answerDiv = document.getElementById(selectedAnswer);
+        updateProgress();
+        currentQuestionIsUnanswered = false;
+        if (answerIsCorrect) {
+            answerDiv.style.backgroundColor = "darkgreen";
+            answerDiv.style.borderLeft = "4px solid black";
+        } else {
+            answerDiv.style.backgroundColor = "darkred";
+            answerDiv.style.borderLeft = "4px solid black";
+        };
+    } else {
+        console.log("Question has already been answered");
+    };
+};
+
+
 
 //EventListeners
-answerA.addEventListener("click", checkAnswer("one"));
-answerB.addEventListener("click", checkAnswer("two"));
-answerC.addEventListener("click", checkAnswer("three"));
-answerD.addEventListener("click", checkAnswer("four"));
+one.addEventListener("click", function() {
+    console.log("Executing answer one...");
+    executeAnswer("one");
+});
+two.addEventListener("click", function() {
+    executeAnswer("two");
+});
+three.addEventListener("click", function() {
+    executeAnswer("three");
+});
+four.addEventListener("click", function() {
+    executeAnswer("four");
+});
 nextButton.addEventListener("click", displayNextQuestion);
-
-
-//How to pass argument into EventListener Function without invoking?
