c3a921f391dd737b10afab96f3b51a91397c717c
Philip <philipadgey@gmail.com>
2021-05-03 12:22:26 +0100

    Extended citizenship quiz

--- script.js
+++ script.js
@@ -30,18 +30,121 @@ const questionsDatabase = {
                 "four": "Every 6 years",
                 correctAnswer: "three" 
             }
+        },
+        "4": {
+            question: "Which charity works to preserve important buildings?",
+            answers: {
+                "one": "NSPCC",
+                "two": "The Red Cross",
+                "three": "Age UK",
+                "four": "The National Trust",
+                correctAnswer: "four" 
+            }
+        },
+        "5": {
+            question: "What is the capital of Scotland?",
+            answers: {
+                "one": "Edinburgh",
+                "two": "London",
+                "three": "Belfast",
+                "four": "Cardiff",
+                correctAnswer: "one" 
+            }
+        },
+        "6": {
+            question: "Which department produces the the official handbook for the test?",
+            answers: {
+                "one": "The English Office",
+                "two": "The Home Office",
+                "three": "The State Office",
+                "four": "The Foreign Office",
+                correctAnswer: "two" 
+            }
+        },
+        "6": {
+            question: "Which department produces the the official handbook for the test?",
+            answers: {
+                "one": "The English Office",
+                "two": "The Home Office",
+                "three": "The State Office",
+                "four": "The Foreign Office",
+                correctAnswer: "two" 
+            }
+        },
+        "7": {
+            question: "In which year was there a major outbreak of plague in London?",
+            answers: {
+                "one": "1465",
+                "two": "1565",
+                "three": "1665",
+                "four": "1765",
+                correctAnswer: "three" 
+            }
+        },
+        "8": {
+            question: "In which county is Stonehenge?",
+            answers: {
+                "one": "Lincolnshire",
+                "two": "Lothians",
+                "three": "Dorset",
+                "four": "Wiltshire",
+                correctAnswer: "four" 
+            }
+        },
+        "9": {
+            question: "What is the name given to rented additional land which people use to grow fruits and vegetables?",
+            answers: {
+                "one": "Allotment",
+                "two": "Roomage",
+                "three": "Spacage",
+                "four": "Garden",
+                correctAnswer: "one" 
+            }
+        },
+        "10": {
+            question: "What do many theatres produce at Christmas time?",
+            answers: {
+                "one": "New Years Celebration",
+                "two": "Tragedy Play",
+                "three": "Huge Display",
+                "four": "Pantomime",
+                correctAnswer: "four" 
+            }
+        },
+        "11": {
+            question: "Who became Prime Minister during WWII?",
+            answers: {
+                "one": "Robert Walpole",
+                "two": "Tony Blair",
+                "three": "Margaret Thatcher",
+                "four": "Winston Churchill",
+                correctAnswer: "four" 
+            }
+        },
+        "12": {
+            question: "Who do you write to to make a complaint about the police?",
+            answers: {
+                "one": "The Chief Constable",
+                "two": "Your local MP",
+                "three": "The House of Lords",
+                "four": "The House of Commons",
+                correctAnswer: "one" 
+            }
         }
     }
 };
 
 //Global variables
 let currentQuestionNumber = 1;
-let numberOfQuestions = 3;
+let numberOfQuestions = Object.keys(questionsDatabase.citizenshipQuiz).length;
 let currentQuestionIsUnanswered = true;
 let numAnsweredQuestions = 0;
 let numCorrectAnswers = 0;
 
 //DOM structure
+const progressTracker = document.getElementById("progressTracker");
+const progressBar = document.getElementById("progressBar");
+const progressPercentage = document.getElementById("progressPercentage")
 const question = document.getElementById("question");
 const questionContainer = document.getElementById("questionContainer");
 const one = document.getElementById("one");
@@ -51,6 +154,27 @@ const four = document.getElementById("four");
 const nextButton = document.getElementById("nextButton");
 
 //Functions
+const loadQuiz = function() {
+    //Generate progress bars
+    for (let i = 1; i < numberOfQuestions + 1; i++) {
+        let generatedDiv = document.createElement("div");
+        generatedDiv.setAttribute("class", "progressNode");
+        generatedDiv.setAttribute("id", `node${i}`);
+        generatedDiv.innerHTML = i;
+        progressBar.appendChild(generatedDiv);
+    };
+
+    //Generate progress percentage
+    let progressString = `Question - ${currentQuestionNumber} of ${numberOfQuestions}`;
+    progressPercentage.innerHTML = progressString;
+
+    //Generate first question
+    question.innerHTML = `${currentQuestionNumber}. ${questionsDatabase.citizenshipQuiz[currentQuestionNumber].question}`;
+    one.innerText = questionsDatabase.citizenshipQuiz[currentQuestionNumber].answers["one"];
+    two.innerText = questionsDatabase.citizenshipQuiz[currentQuestionNumber].answers["two"];
+    three.innerText = questionsDatabase.citizenshipQuiz[currentQuestionNumber].answers["three"];
+    four.innerText = questionsDatabase.citizenshipQuiz[currentQuestionNumber].answers["four"];
+};
 const resetQuestionStyling = function() {
     one.style.border = "medium none";
     one.style.backgroundColor = "rgb(75, 75, 75)";
@@ -63,8 +187,10 @@ const resetQuestionStyling = function() {
 };
 const displayNextQuestion = function() {
     if (currentQuestionNumber === numberOfQuestions) {
-        console.log("Reached the end of the quiz");
+        alert("Reached the end of the quiz");
         return
+    } else if (currentQuestionIsUnanswered === true) {
+        alert("Answer the question before moving on!");
     } else {
         console.log("Changing currentQuestionIsUnanswered to true...");
         currentQuestionNumber++;
@@ -93,13 +219,13 @@ const generateQuestionHTML = function(number) {
 };
 const updateProgress = function(answerIsCorrect) {
     //Progress Bar
-    let currentQuestionBar = document.getElementById(`bar${currentQuestionNumber}`);
+    let currentQuestionNode = document.getElementById(`node${currentQuestionNumber}`);
     if (answerIsCorrect === true) {
-        currentQuestionBar.style.backgroundColor = "green";
+        currentQuestionNode.style.backgroundColor = "green";
     } else {
-        currentQuestionBar.style.backgroundColor = "red";
+        currentQuestionNode.style.backgroundColor = "red";
     };
-    
+
     //Score 
     let percentageCorrect = Math.round(numCorrectAnswers / numAnsweredQuestions * 100);
     let progressString = 
@@ -137,8 +263,6 @@ const executeAnswer = function(selectedAnswer) {
     };
 };
 
-
-
 //EventListeners
 one.addEventListener("click", function() {
     console.log("Executing answer one...");
@@ -154,3 +278,6 @@ four.addEventListener("click", function() {
     executeAnswer("four");
 });
 nextButton.addEventListener("click", displayNextQuestion);
+
+//Generate Quiz
+loadQuiz();
